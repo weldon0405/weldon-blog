df67d3338abf0b44e4b931b2e425c9c20ed5320f
Weldon <weldon0405@gmail.com>
2017-08-30 23:45:50 -0400

    Register routes built, Registration page constructed.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from app import db
 from sqlalchemy import Column, DateTime
 from datetime import datetime
 
+
+
 class Blog(db.Model):
 
     __tablename__ = "articles"
@@ -21,4 +23,27 @@ class Blog(db.Model):
 
     def __repr__(self):
         return 'title {}'.format(self.name)
+
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_plaintext = db.Column(db.String, nullable=False) ######TEMPORARY -- MUST HASH THE PASSWORD AND DELETE
+
+    def __init__(self, username, first_name, last_name, email, password_plaintext):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password_plaintext = password_plaintext
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.name)
     
